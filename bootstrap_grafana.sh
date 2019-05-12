eeb98ee5bbfb1c274fbbdc70e9d7e1f7a66e7833
Vitaly Bichov <vitalyb@wix.com>
2019-05-12 15:21:34 +0300

    adding ifupifdown

--- bootstrap_grafana.sh
+++ bootstrap_grafana.sh
@@ -6,7 +6,7 @@ GRAFANA_VERSION="6.1.6"
 
 # update apt
 apt-get update --quiet
-apt-get install -y unzip git apt-transport-https --quiet
+apt-get install -y unzip git ifupdown apt-transport-https --quiet
 
 # install the Grafana PGP Key and repo
 curl -s https://packages.grafana.com/gpg.key | sudo apt-key add -
@@ -14,7 +14,7 @@ add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 
 # install the elk PGP Key and repo
 curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-add-apt-rrepository "deb https://artifacts.elastic.co/packages/7.x/apt stable main" 
+add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main" 
 
 apt-get update --quiet
 echo "[*] Installing Grafana"
